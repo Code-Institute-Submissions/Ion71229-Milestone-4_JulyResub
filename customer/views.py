09c7d4a3f34af66fe8f61fd01bc06596089ac1b5
Ion71229 <ionsimion279@gmail.com>
2022-01-30 13:52:34 +0000

    add the menu items

--- customer/views.py
+++ customer/views.py
@@ -10,3 +10,52 @@ class About(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+
+class Order(View):
+
+    def get(self, request, *args, **kwargs):
+        starter = MenuItem.objects.filter(category__name__contains='Starter')
+        maincourse = MenuItem.objects.filter(category__name__contains='Main Course')
+        dessert = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drinks')
+
+        context = {
+            'Starter': starter,
+            'Main Course': maincourse,
+            'Dessert': dessert,
+            'Drinks': drinks,
+        }
+        return render(request, 'customer/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+            order_items['items'].append(item_data)
+        
+        price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+        
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
--- fooddelivery/urls.py
+++ fooddelivery/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from customer.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
