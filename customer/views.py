4f81292caf5ea1f27fac37fb02708b081bcfcb80
Ion71229 <ionsimion279@gmail.com>
2022-02-08 13:24:10 +0000

    deploy to heroku the app

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Q
 from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
 
@@ -90,3 +91,28 @@ class Order(View):
         }
 
         return render(request, 'customer/order_confirmation.html', context)
+
+class Menu(View):
+    def get(self, request, *args, **kwargs):
+        menu_items = MenuItem.objects.all()
+
+        context = {
+            'menu_items': menu_items,
+        }
+
+        return render(request, 'customer/menu.html', context)
+
+class MenuSearch(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get("q")
+
+        menu_items = MenuItem.objects.filter(
+            Q(name__icontains=query) |
+            Q(price__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        context = {
+            'menu_items': menu_items
+        }
+        return render(request, 'customer/menu.html', context)
