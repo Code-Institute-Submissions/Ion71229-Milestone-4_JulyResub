79304ca1eb347924746364d15478d64b8850323f
Ion71229 <ionsimion279@gmail.com>
2022-02-15 12:25:24 +0000

    change the debug to false

--- fooddelivery/settings.py
+++ fooddelivery/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--km$!2dkj8fn-pb!90v1fxnr_$%*z4%-a-$%^pgl2ud)m6@!ue')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
